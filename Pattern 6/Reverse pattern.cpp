4cd7b432cebc1e5bd3e2bd36738682baa6ad4ecd
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-05-30 19:10:59 +0530

    Reverse pattern

---
+++ Pattern 6/Reverse pattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col;
+    int n;
+    cout<<"Enter the number of columns : ";
+    cin>>n;
+    for(row = n;row >= 1;row--,cout<<"\n"){
+        for(col = 1;col <= row;col++){
+            cout<<col<<" ";
+        }
+    }
+
+    return 0;
+}
