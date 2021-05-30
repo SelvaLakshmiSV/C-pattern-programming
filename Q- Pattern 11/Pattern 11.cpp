04a1741aa2cd0497cbb83c13907a6650dc322f8f
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-05-30 20:27:26 +0530

    Pattern 11

---
+++ Q- Pattern 11/Pattern 11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col;
+    int n;
+    cout<<"Enter the number of columns : ";
+    cin>>n;
+    for(row = 1;row <= n;row++,cout<<"\n"){
+        for(col = 1;col <= row;col++){
+            cout<<row<<""<<col<<" ";
+        }
+    }
+     for(row = n-1;row >= 1;row--,cout<<"\n"){
+        for(col = 1;col <= row;col++){
+            cout<<row<<""<<col<<" ";
+        }
+    }
+
+    return 0;
+}
